ce36b079c1139959ccb78081bc9d86083498879e
Lê Thái Khôi Nguyên <97087502+XIVIXMMI@users.noreply.github.com>
2022-12-31 02:01:14 +0700

    if_statements

--- if_ statements.py
+++ if_ statements.py
@@ -2,5 +2,11 @@
 
 age = int(input("How old are you ? : "))
 
-if age >= 18:
+if age == 100:
+    print("you are a century old!")
+elif age >= 18:
     print("you are an adult!")
+elif age < 0:
+    print("you haven't been born yet bro!")
+else:
+    print("you just a child!")
