522564ec0a019acfc4356b2ed00da21b0c7db1ee
Lê Thái Khôi Nguyên <97087502+XIVIXMMI@users.noreply.github.com>
2023-01-06 02:42:34 +0700

    sets

---
+++ sets.py
@@ -0,0 +1,17 @@
+# set = collection which is unordered, unindexed. No duplicate values
+
+utensils = {"fork","spoon","knife"}
+dishes = {"bowl","plate","cup","knife"}
+
+#utensils.add("napkin")
+#tensils.remove("fork")
+#utensils.clear()
+#utensils.update(dishes)
+#dinner_table = utensils.union(dishes)
+
+#print(utensils.difference(dishes))
+#print(dishes.difference(utensils))
+
+print(dishes.intersection(utensils))
+#for x in dinner_table:
+    #print(x)
