9ef14aa495a4d5c886ca334116f01a1408787f7d
Lê Thái Khôi Nguyên <97087502+XIVIXMMI@users.noreply.github.com>
2023-01-07 15:15:17 +0700

    indexing

---
+++ indexing.py
@@ -0,0 +1,14 @@
+#index operator [] = gives access to sequence's element (str,list,tuples)
+
+name = "nguyen le"
+
+#if (name[0].islower()):
+    #name = name.capitalize()
+
+first_name = name[:6].upper()
+last_name = name[7:].upper()
+last_character = name[-1]
+
+print(first_name)
+print(last_name)
+print(last_character)
