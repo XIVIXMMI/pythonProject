7a5e03ac41efeb33de8d68afbb4159a158e763a6
Lê Thái Khôi Nguyên <97087502+XIVIXMMI@users.noreply.github.com>
2023-01-07 15:37:54 +0700

    return_statement

---
+++ return_statement.py
@@ -0,0 +1,10 @@
+#return statement = Functions send Python values/object back to the caller
+#                   these value/object are known as the function's return value
+
+def multiplt(number1, number2):
+    return number1 * number2
+
+
+x = multiplt(5,6)
+
+print(x)
