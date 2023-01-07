1ee98d776b0f936969359e86da8a8df2b38f13f4
Lê Thái Khôi Nguyên <97087502+XIVIXMMI@users.noreply.github.com>
2023-01-07 15:29:28 +0700

    functions

---
+++ functions.py
@@ -0,0 +1,9 @@
+# function = a block of code which is executed only when it is called
+#            một khối lệnh chỉ được thực hiện khi gọi nó
+
+def hello(first_name, last_name, age):
+    print("Hello "+first_name+" "+last_name)
+    print("You are "+str(age)+" year old")
+    print("Have a nice day!!")
+
+hello("Nguyen","Le", 21)
